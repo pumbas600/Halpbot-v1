431622c65bb2298d8cd90128ae60e0400b07904a
Pumbas600 <joshjeffers600@gmail.com>
2021-10-07 12:09:35 +1300

    Added an ExpiringMap implementation. Tests will be added later

--- HalpBot-Core/src/main/java/nz/pumbas/halpbot/actions/annotations/Cooldown.java
+++ HalpBot-Core/src/main/java/nz/pumbas/halpbot/actions/annotations/Cooldown.java
@@ -13,8 +13,8 @@ public @interface Cooldown
     long duration() default 5;
 
     /**
-     * NOTE: You can't use a time unit smaller than seconds. Trying to do so will cause an error to be thrown when
-     * generating the action.
+     * NOTE: You can't use a time unit smaller than milliseconds. Trying to specify a time in microseconds or
+     * nanoseconds will result in an error.
      */
     TimeUnit unit() default TimeUnit.SECONDS;
 }
