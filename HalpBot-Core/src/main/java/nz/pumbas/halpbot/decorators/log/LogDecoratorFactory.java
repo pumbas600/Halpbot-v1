98bd28b3d0aeef7e3c4b3e279de4b24ec649b3aa
Pumbas600 <joshjeffers600@gmail.com>
2021-12-27 14:57:03 +1300

    Added missing @Service to factories

--- HalpBot-Core/src/main/java/nz/pumbas/halpbot/decorators/log/LogDecoratorFactory.java
+++ HalpBot-Core/src/main/java/nz/pumbas/halpbot/decorators/log/LogDecoratorFactory.java
@@ -1,8 +1,11 @@
 package nz.pumbas.halpbot.decorators.log;
 
+import org.dockbox.hartshorn.core.annotations.service.Service;
+
 import nz.pumbas.halpbot.actions.invokable.ActionInvokable;
 import nz.pumbas.halpbot.decorators.ActionInvokableDecoratorFactory;
 
+@Service
 public interface LogDecoratorFactory extends ActionInvokableDecoratorFactory<LogDecorator<?>, Log>
 {
     //@Factory
--- HalpBot-Core/src/main/java/nz/pumbas/halpbot/permissions/PermissionDecoratorFactory.java
+++ HalpBot-Core/src/main/java/nz/pumbas/halpbot/permissions/PermissionDecoratorFactory.java
@@ -1,10 +1,13 @@
 package nz.pumbas.halpbot.permissions;
 
+import org.dockbox.hartshorn.core.annotations.service.Service;
+
 import nz.pumbas.halpbot.actions.invokable.ActionInvokable;
 import nz.pumbas.halpbot.decorators.ActionInvokableDecoratorFactory;
 import nz.pumbas.halpbot.permissions.Permission;
 import nz.pumbas.halpbot.permissions.PermissionDecorator;
 
+@Service
 public interface PermissionDecoratorFactory extends ActionInvokableDecoratorFactory<PermissionDecorator<?>, Permission>
 {
     //@Factory
