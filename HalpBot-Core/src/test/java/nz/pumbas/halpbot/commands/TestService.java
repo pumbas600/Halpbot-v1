07821bc20991e3d887f6718bbac10acd7454f2ea
Pumbas600 <joshjeffers600@gmail.com>
2022-01-26 10:39:58 +1300

    Small change to test to better illustrate issue

--- HalpBot-Core/src/test/java/nz/pumbas/halpbot/commands/TestService.java
+++ HalpBot-Core/src/test/java/nz/pumbas/halpbot/commands/TestService.java
@@ -3,8 +3,6 @@ package nz.pumbas.halpbot.commands;
 import org.dockbox.hartshorn.core.annotations.stereotype.Service;
 import org.dockbox.hartshorn.core.context.ApplicationContext;
 
-import java.util.Objects;
-
 import javax.inject.Inject;
 
 @Service
@@ -13,9 +11,8 @@ public class TestService
     @Inject
     private ApplicationContext applicationContext;
 
-    public boolean test() {
-        TestService testService = this.applicationContext.get(TestService.class);
-        return this.equals(testService);
+    public boolean test(TestService self) {
+        return this.equals(self);
     }
 
 }
