be3b8048c4c886f2d09ac9d2b51125fcccc924e2
Pumbas600 <joshjeffers600@gmail.com>
2022-02-04 11:56:29 +1300

    #17 Fixed tests and hopefully fixed workflow

--- HalpBot-Core/src/test/java/nz/pumbas/halpbot/commands/TestUtil.java
+++ HalpBot-Core/src/test/java/nz/pumbas/halpbot/commands/TestUtil.java
@@ -3,8 +3,10 @@ package nz.pumbas.halpbot.commands;
 import org.dockbox.hartshorn.core.context.element.MethodContext;
 import org.dockbox.hartshorn.core.context.element.TypeContext;
 
-public class TestUtil
+public final class TestUtil
 {
+    private TestUtil() {}
+
     public static MethodContext<?, ?> method(Class<?> type, String name) {
         return TypeContext.of(type).methods()
                 .stream()
