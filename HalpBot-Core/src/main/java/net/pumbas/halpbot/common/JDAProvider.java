385b0f6d3a97cc958fcface28ad53cb8a489bdf2
pumbas600 <joshjeffers600@gmail.com>
2022-06-13 12:08:58 +1200

    Made JDAProvider use manual singleton

--- HalpBot-Core/src/main/java/net/pumbas/halpbot/common/JDAProvider.java
+++ HalpBot-Core/src/main/java/net/pumbas/halpbot/common/JDAProvider.java
@@ -4,6 +4,7 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.pumbas.halpbot.configurations.BotConfiguration;
 
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.dockbox.hartshorn.application.context.ApplicationContext;
 import org.dockbox.hartshorn.component.Service;
 import org.dockbox.hartshorn.component.processing.Provider;
@@ -12,28 +13,30 @@ import java.util.function.Function;
 
 import javax.security.auth.login.LoginException;
 
-import jakarta.inject.Singleton;
-
 @Service
 public class JDAProvider {
 
+    @Nullable
+    private JDA jdaInstance;
+
     @Provider
-    @Singleton
     public JDA jdaProvider(final ApplicationContext applicationContext, final BotConfiguration botConfiguration)
         throws LoginException, InterruptedException
     {
-        final Function<String, JDABuilder> jdaBuilder = HalpbotBuilder.instance().jdaBuilder();
-        if (jdaBuilder == null) {
-            throw new IllegalStateException("JDABuilder has not been set");
+        if (this.jdaInstance == null) {
+            final Function<String, JDABuilder> jdaBuilder = HalpbotBuilder.instance().jdaBuilder();
+            if (jdaBuilder == null) {
+                throw new IllegalStateException("JDABuilder has not been set");
+            }
+
+            applicationContext.log().info("Constructing JDA instance");
+
+            final JDABuilder builder = jdaBuilder.apply(botConfiguration.token());
+            this.jdaInstance = builder.build();
+            this.jdaInstance.awaitReady();
+
+            applicationContext.log().info("JDA instance created");
         }
-
-        applicationContext.log().info("Constructing JDA instance");
-
-        final JDABuilder builder = jdaBuilder.apply(botConfiguration.token());
-        final JDA jda = builder.build();
-        jda.awaitReady();
-
-        applicationContext.log().info("JDA instance created");
-        return jda;
+        return this.jdaInstance;
     }
 }
